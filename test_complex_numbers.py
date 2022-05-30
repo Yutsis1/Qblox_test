09f72161df7488b085ca20de7c4a8bf9728a9dd1
Roman Iutsis <yutsis5@gmail.com>
2022-05-30 10:11:38 +0200

    test_complex_numbers.py: added docstrings and TODO

--- test_complex_numbers.py
+++ test_complex_numbers.py
@@ -9,6 +9,10 @@ class TestComplexNumbers:
 
     @pytest.fixture
     def create_data_for_test(self):
+        """
+        pytest fixture which prepare 2 random complex numbers for tests
+        :return:
+        """
         complex_number_1 = ComplexNumber(
             random.uniform(1, 10),
             random.uniform(1, 10)
@@ -23,7 +27,7 @@ class TestComplexNumbers:
 
     def test_sum(self, create_data_for_test):
         """
-
+        test for sum
         :return:
         """
         complex_number_1, complex_number_2 = create_data_for_test[0], create_data_for_test[1]
@@ -34,7 +38,7 @@ class TestComplexNumbers:
 
     def test_sub(self, create_data_for_test):
         """
-
+        test for sub
         :param create_data_for_test:
         :return:
         """
@@ -43,3 +47,5 @@ class TestComplexNumbers:
         print(complex_res)
         assert complex_res.real_part == complex_number_1.real_part - complex_number_2.real_part
         assert complex_res.imaginary_part == complex_number_1.imaginary_part - complex_number_2.imaginary_part
+
+    # TODO Add test for multiplication and division
